2b81c88817d1d5b35573ef730a26ce95a6dcbe6b
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-19 18:00:38 -0300

    wip

--- src/Application/Integration/DTO/ResponseKeyValueOutput.php
+++ src/Application/Integration/DTO/ResponseKeyValueOutput.php
@@ -6,7 +6,7 @@ namespace CodePix\Bank\Application\Integration\DTO;
 
 class ResponseKeyValueOutput
 {
-    public function __construct(public string $id, public string $key)
+    public function __construct(public string $id, public string $key, public int $status)
     {
         //
     }
