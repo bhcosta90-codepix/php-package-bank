82f133cfd54fb25fbf67e6fa66b8e209711c70b4
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-19 18:03:33 -0300

    wip

--- src/Application/Integration/Response/ResponseKeyValueOutput.php
+++ src/Application/Integration/Response/ResponseKeyValueOutput.php
@@ -6,7 +6,7 @@ namespace CodePix\Bank\Application\Integration\Response;
 
 class ResponseKeyValueOutput
 {
-    public function __construct(public string $id, public string $key, public int $status)
+    public function __construct(public ?string $id, public ?string $key, public int $status)
     {
         //
     }
